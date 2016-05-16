fab1986b5e30eb7466d9d0cda19c7b75fc95c108
DannyFeliz <dannyfeliz08@gmail.com>
2016-05-15 20:32:54 -0400

    Initial commmit

---
+++ SQL/provincias.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 5.6.24, for Win32 (x86)
+--
+-- Host: 127.0.0.1    Database: rd
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.1.9-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `provincias`
+--
+
+LOCK TABLES `provincias` WRITE;
+/*!40000 ALTER TABLE `provincias` DISABLE KEYS */;
+INSERT INTO `provincias` VALUES (1,'Distrito Nacional'),(21,'San Pedro de Macorís'),(22,'La Romana'),(23,'La Altagracia'),(24,'El Seibo'),(25,'Hato Mayor'),(31,'Duarte'),(32,'Samaná'),(33,'Maria Trinidad Sánchez'),(34,'Salcedo'),(41,'La Vega'),(42,'Monseñor Nouel'),(43,'Sánchez Ramirez'),(51,'Santiago'),(56,'Espaillat'),(57,'Puerto Plata'),(61,'Valverde'),(62,'Monte Cristi'),(63,'Dajabónn'),(64,'Santiago Rodríguez'),(71,'Azua'),(72,'San Juan de la Maguana'),(73,'Elías Piña'),(81,'Barahona'),(82,'Bahoruco'),(83,'Independencia'),(84,'Perdenales'),(91,'San Cristóbal'),(92,'Monte Plata'),(93,'San José de Ocoa'),(94,'Peravia');
+/*!40000 ALTER TABLE `provincias` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-05-15 20:29:26
